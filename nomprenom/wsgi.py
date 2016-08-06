38987c435edb4782d59170d88da13b55bd538c00
Sushinski <pavel.sushi@gmail.com>
2016-08-07 00:08:19 +0400

    change application

--- nomprenom/wsgi.py
+++ nomprenom/wsgi.py
@@ -13,4 +13,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nomprenom.settings")
 
-application = get_wsgi_application()
+def application(environ, start_response):
+    status = '200 OK'
+    output = 'Hello World!'
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+
+    return [output]
