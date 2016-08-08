f19bdd9df57bdb20d554bbb3621db81611424e54
Sushinski <pavel.sushi@gmail.com>
2016-08-08 19:22:27 +0300

    update wsgi

--- nomprenom/wsgi.py
+++ nomprenom/wsgi.py
@@ -9,11 +9,17 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 import sys
-
+import site
 from django.core.wsgi import get_wsgi_application
+
+site.addsitedir('/home/sushinski/virtualenvs/npn/npnenv/lib/python3.5/site-packages')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
+activate_env=os.path.expanduser("/home/sushinski/virtualenvs/npnenv/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nomprenom.settings")
 
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
