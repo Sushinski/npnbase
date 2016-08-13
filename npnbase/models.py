8cae7c9c4a8ed217170d368f349b54cc1b766c1d
Paul Sushinski <pavel.sushi@gmail.com>
2016-08-14 01:27:03 +0300

    adjusting models

--- npnbase/models.py
+++ npnbase/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class GroupRecord(models.Model):
+    _id = models.PositiveIntegerField(primary_key=True)
+    group_name = models.TextField(verbose_name='group_name')
+
+
+class NameRecord(models.Model):
+    _id = models.PositiveIntegerField(primary_key=True)
+    name = models.TextField(verbose_name='name', unique=True)
+    sex = models.PositiveIntegerField(verbose_name='sex')
+    selected = models.IntegerField(verbose_name='selected', default=0)
+    description = models.TextField(verbose_name='description')
+
+
+class NameGroupRecord(models.Model):
+    _id = models.PositiveIntegerField(primary_key=True)
+    group_id = models.ForeignKey(GroupRecord, on_delete=models.CASCADE)
+    name_id = models.ForeignKey(NameRecord, on_delete=models.CASCADE)
+
+
+class ZodiacRecord(models.Model):
+    _id = models.PositiveIntegerField(primary_key=True)
+    zod_month = models.PositiveSmallIntegerField(unique=True, verbose_name='zod_month')
+    zod_sign = models.TextField(unique=True, verbose_name='zod_sign')
+
+
+class NameZodiacRecord(models.Model):
+    _id = models.PositiveIntegerField(primary_key=True)
+    name_id = models.ForeignKey(NameRecord, on_delete=models.CASCADE)
+    zodiac_id = models.ForeignKey(ZodiacRecord, on_delete=models.CASCADE)
