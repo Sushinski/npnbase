bc16854ad3f1e350fab7f8223c69baad7d2f6520
Golubev <pavel.sushi@gmail.com>
2016-08-18 17:55:19 +0300

    default database is mysql now

--- nomprenom/settings.py
+++ nomprenom/settings.py
@@ -76,10 +76,19 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portal_db',
+        'TEST_NAME': 'test_portal_db',
+        'USER': 'npnadmin',
+        'PASSWORD': '2meski4n1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
