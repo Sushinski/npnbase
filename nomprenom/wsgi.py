faab77b80eb113e24541554b01d54b90170e1cd9
Golubev <pavel.sushi@gmail.com>
2016-08-19 17:31:29 +0300

    editing paths

--- nomprenom/wsgi.py
+++ nomprenom/wsgi.py
@@ -12,11 +12,11 @@ import sys
 import site
 from django.core.wsgi import get_wsgi_application
 
-# site.addsitedir('/home/sushinski/virtualenvs/npn/npnenv/lib/python3.5/site-packages')
+site.addsitedir('/home/sushinski/virtualenvs/npn/npnenv/lib/python3.5/site-packages')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
-activate_env=os.path.expanduser("/home/sushinski/virtualenvs/npn/npnenv/bin/activate_this.py")
+activate_env = os.path.expanduser("~sushinski/virtualenvs/npn/npnenv/bin/activate_this.py")
 exec(open(activate_env).read())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nomprenom.settings")
