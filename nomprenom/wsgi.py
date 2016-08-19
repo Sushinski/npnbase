eba28b6bdd47fa4642792b64bf3b6392bfaf3841
Golubev <pavel.sushi@gmail.com>
2016-08-19 16:50:23 +0300

    comment addsitedir

--- nomprenom/wsgi.py
+++ nomprenom/wsgi.py
@@ -12,7 +12,7 @@ import sys
 import site
 from django.core.wsgi import get_wsgi_application
 
-site.addsitedir('/home/sushinski/virtualenvs/npn/npnenv/lib/python3.5/site-packages')
+# site.addsitedir('/home/sushinski/virtualenvs/npn/npnenv/lib/python3.5/site-packages')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
