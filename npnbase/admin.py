41af949329d381f5ffa9257251aebb154aa9b5fd
Golubev <pavel.sushi@gmail.com>
2016-12-05 19:04:36 +0300

    add group admin

--- npnbase/admin.py
+++ npnbase/admin.py
@@ -14,4 +14,14 @@ class NameRecordAdmin(admin.ModelAdmin):
     inlines = [ZodiacsInline]
 
 
+class GroupRecordAdmin(admin.ModelAdmin):
+    fields = ['group_name']
+    list_display = ('group_name',)
+    search_fields = ['group_name']
+
 admin.site.register(NameRecord, NameRecordAdmin)
+admin.site.register(GroupRecord, GroupRecordAdmin)
+
+
+admin.site.site_title = u'NomPreNom Admin'
+admin.site.site_header = u'NomPreNom Admin'
