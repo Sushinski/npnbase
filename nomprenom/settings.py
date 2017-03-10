721afafd74764542fd260ca4f426e97630ffa416
Golubev <pavel.sushi@gmail.com>
2017-03-10 19:17:29 +0300

    fixing models(unicode str support) & JSON_UNICODE rest pref

--- nomprenom/settings.py
+++ nomprenom/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-DEFAULT_CHARSET = 'utf-8'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -139,5 +138,6 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         # 'rest_framework.renderers.BrowsableAPIRenderer',
-    )
+    ),
+    'UNICODE_JSON': True,
 }
--- npnbase/models.py
+++ npnbase/models.py
@@ -1,53 +1,60 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class GroupRecord(models.Model):
     _id = models.AutoField(primary_key=True)
-    group_name = models.TextField(verbose_name='group_name', unique=True)
+    group_name = models.CharField(max_length=64, verbose_name='group_name', unique=True)
+
+    def __str__(self):
+        return self.group_name
 
     class Meta:
         db_table = 'GroupRecord'
 
 
+@python_2_unicode_compatible
 class ZodiacRecord(models.Model):
     _id = models.AutoField(primary_key=True)
     zod_month = models.PositiveSmallIntegerField(unique=True, verbose_name='zod_month')
-    zod_sign = models.TextField(unique=True, verbose_name='zod_sign')
+    zod_sign = models.CharField(max_length=64, unique=True, verbose_name='zod_sign')
 
     def __str__(self):
-        return str(self.zod_sign)
-
-    def __unicode__(self):
-        return u'%s' % self.zod_sign
+        return self.zod_sign
 
     class Meta:
         db_table = 'ZodiacRecord'
 
 
-class NameZodiacRecord(models.Model):
-    _id = models.AutoField(primary_key=True)
-    name_id = models.ForeignKey('NameRecord', on_delete=models.CASCADE)
-    zodiac_id = models.ForeignKey(ZodiacRecord, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'NameZodiacRecord'
-
-
+@python_2_unicode_compatible
 class NameRecord(models.Model):
     GENDER_CHOICE = (
         (1, 'Boy'),
         (0, 'Girl')
     )
     _id = models.AutoField(primary_key=True)
-    name = models.TextField(verbose_name='name', unique=True)
+    name = models.CharField(max_length=64, verbose_name='name', unique=True)
     selected = models.IntegerField(verbose_name='selected', default=0)
     description = models.TextField(verbose_name='description', null=True)
     groups = models.ForeignKey(GroupRecord, blank=True, null=True, on_delete=models.SET_NULL)
     sex = models.PositiveIntegerField(verbose_name='sex', choices=GENDER_CHOICE, default=1)
-    zodiacs = models.ManyToManyField(ZodiacRecord, through=NameZodiacRecord, through_fields=('name_id', 'zodiac_id'))
+    zodiacs = models.ManyToManyField(ZodiacRecord, through='NameZodiacRecord', through_fields=('name_id', 'zodiac_id'))
 
     class Meta:
         db_table = 'NameRecord'
+
+    def __str__(self):
+        return self.name
+
+
+class NameZodiacRecord(models.Model):
+    _id = models.AutoField(primary_key=True, default=None)
+    name_id = models.ForeignKey(NameRecord, on_delete=models.CASCADE)
+    zodiac_id = models.ForeignKey(ZodiacRecord, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'NameZodiacRecord'
 
 
 class PrefRecord(models.Model):
